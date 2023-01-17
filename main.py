bf3f6c9c1bcb5570868b3940cc52011a9cc012d1
Rithik Raja <rithik@Rithiks-MacBook-Pro.local>
2023-01-17 17:39:48 -0500

    change num epochs

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ import mnist_loader
 
 loader = mnist_loader.Loader()
 classifier = network.Network([28 * 28, 30, 10])
-classifier.stochastic_gradient_descent(loader.load_train_images(), loader.load_train_labels(), 5, 3)
+classifier.stochastic_gradient_descent(loader.load_train_images(), loader.load_train_labels(), 30, 3)
 
 print("Complete. Accuracy: {} / 1000".format(classifier.evaluate_performance(loader.load_test_images(), loader.load_test_labels())))
